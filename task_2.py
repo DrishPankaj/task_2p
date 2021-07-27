2c911aa31f70d72e56871b9b87147e484bdab014
dr58 <drishpankaj@gmail.com>
2021-07-27 16:54:55 +0530

    task 2

---
+++ task_2.py
@@ -0,0 +1,24 @@
+import sched, datetime, time, os
+x = datetime.datetime.now()
+print("Start_Time = ", x )
+s = sched.scheduler(time.time, time.sleep)
+#File create
+#os.remove("test.txt")
+if os.path.isfile("test.txt"):
+    file = open("test.txt","a")
+else:
+    file = open("test.txt", "x")
+
+def add_newLine(sc): 
+
+#print("...Add a New line...", datetime.datetime.now())
+    if os.stat("test.txt").st_size == 0:
+        with open("test.txt","w") as file1:
+            file1.write("Added a New line at time : "+str(datetime.datetime.now())+ "\n")
+    else:
+        with open("test.txt","a") as file1:
+            file1.write("Added a New line at time : "+str(datetime.datetime.now())+ "\n")
+    s.enter(1, 1, add_newLine, (sc,))
+
+s.enter(1, 1, add_newLine, (s,))
+s.run()
